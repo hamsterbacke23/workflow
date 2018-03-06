3c098ded44a922efcf76f0554631099c195b7e80
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-03-07 00:23:38 +0100

    Add instructions

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -14,9 +14,27 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Bar engine={this.state.engine} />
         <Diagram setEngine={this.setEngine} />
+        <div className="instructions">
+          <h2>Usage</h2>
+          <ul>
+            <li>
+              <strong>Zoom in and out</strong>: Use mousewheel
+            </li>
+            <li>
+              <strong>Select</strong>: Click single nodes or links
+            </li>
+            <li>
+              <strong>Select multiple</strong>: Hold shift and use mouse
+            </li>
+            <li>
+              <strong>Delete</strong>: Select nodes and hit 'delete' on your
+              keyboard
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
